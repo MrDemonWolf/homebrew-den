9f9d112142443668779ebf905f6d36e7a12676b8
Nathanial Henniges <19924836+nathanialhenniges@users.noreply.github.com>
2026-03-30 00:18:56 -0500

    chore: update WolfWave cask to v1.0.0

--- Casks/wolfwave.rb
+++ Casks/wolfwave.rb
@@ -1,5 +1,5 @@
 cask "wolfwave" do
-  version "1.0.0-rc.1"
+  version "1.0.0"
   sha256 :no_check
 
   url "https://github.com/MrDemonWolf/wolfwave/releases/download/v#{version}/WolfWave-#{version}-arm64.dmg"
